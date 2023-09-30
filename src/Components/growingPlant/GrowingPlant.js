4d817898c62ef79094a47cdae87502c5ac7c1193
Rithika <143435061+Rithikasingaravel@users.noreply.github.com>
2023-09-30 06:38:11 +0000

    Update 3 files

---
+++ src/Components/growingPlant/GrowingPlant.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./GrowingPlant.css";
+
+function GrowingPlant() {
+  return <div className="plant">Growing Plant</div>;
+}
+
+export default GrowingPlant;
--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import GrowingPlant from "../growingPlant/GrowingPlant";
 
 function Home() {
-  return <div>Hi Guys,Welcome</div>;
+  return;
+  <div>
+    <h1>Hi Guys,Welcome</h1>
+    <GrowingPlant />
+  </div>;
 }
 
 export default Home;
